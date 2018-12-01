82b1a6dff96fa2c7c029936fa7776f7680776ee3
Sebastian Schepis <sschepis@blockchainfoundry.co>
2018-12-01 16:34:54 -0700

    major refactor

---
+++ emulatorview/src/main/java/linuxdroid/androidterm/emulatorview/compat/ClipboardManagerCompat.java
@@ -0,0 +1,9 @@
+package linuxdroid.androidterm.emulatorview.compat;
+
+public interface ClipboardManagerCompat {
+	CharSequence getText();
+
+	boolean hasText();
+
+    void setText(CharSequence text);
+}
