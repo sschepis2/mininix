fe06549d750177ebcb82410417a4829d0e3e55f8
Sebastian Schepis <sschepis@blockchainfoundry.co>
2018-12-01 19:49:36 -0700

    I think I have it

--- term/src/main/jni/Android.mk
+++ term/src/main/jni/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := emulatorview
+LOCAL_MODULE := libmininix-androidterm5
 LOCAL_LDFLAGS := -Wl,--build-id
 LOCAL_SRC_FILES := \
-	/home/sschepis/media/linuxdroid/mininix/emulatorview/src/main/jni/Application.mk \
+	/home/sschepis/media/linuxdroid/mininix/Application.mk \
 
-LOCAL_C_INCLUDES += /home/sschepis/media/linuxdroid/mininix/emulatorview/src/debug/jni
-LOCAL_C_INCLUDES += /home/sschepis/media/linuxdroid/mininix/emulatorview/src/main/jni
+LOCAL_C_INCLUDES += /home/sschepis/media/linuxdroid/mininix/term/src/debug/jni
+LOCAL_C_INCLUDES += /home/sschepis/media/linuxdroid/mininix/term/src/main/jni
 
 include $(BUILD_SHARED_LIBRARY)
